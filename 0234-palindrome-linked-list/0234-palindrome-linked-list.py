34737b8ba8473bdb38d9d52545481847f85e496c
Sourav24hub <souravkum2006@gmail.com>
2026-07-30 23:41:35 +0530

    Time: 123 ms (78.08%), Space: 83.5 MB (42.26%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,5 +5,11 @@ class Solution(object):
         while curr:
             lis.append(curr.val)
             curr = curr.next
-        rev = lis[::-1]
-        return rev == lis
+        left = 0
+        right = len(lis) - 1
+        while left <= right:
+            if lis[left] != lis[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
