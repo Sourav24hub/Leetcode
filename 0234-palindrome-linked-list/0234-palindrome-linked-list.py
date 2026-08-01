407d9bdd97fa990438973270cd51a3dcd524969f
Sourav24hub <souravkum2006@gmail.com>
2026-08-01 13:51:52 +0530

    Time: 111 ms (97.17%), Space: 84.5 MB (11.72%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -1,23 +1,8 @@
 class Solution(object):
     def isPalindrome(self, head):
-        if not head or not head.next:
-            return True
-        slow = fast = head
-        while fast.next and fast.next.next:
-            slow = slow.next
-            fast = fast.next.next
-        prev = None
-        curr = slow.next
+        lis = []
+        curr = head
         while curr:
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt
-        first = head
-        second = prev
-        while second:
-            if first.val != second.val:
-                return False
-            first = first.next
-            second = second.next
-        return True
+            lis.append(curr.val)
+            curr = curr.next
+        return lis == lis[::-1]
