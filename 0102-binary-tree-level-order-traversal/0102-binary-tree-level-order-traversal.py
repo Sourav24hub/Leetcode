d0362ae8aa538df200c3855740b580d66d2acc85
Sourav24hub <souravkum2006@gmail.com>
2026-08-19 13:02:47 +0530

    Time: 7 ms (6.59%), Space: 13.4 MB (11.05%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -0,0 +1,49 @@
+class LQueue:
+    def __init__(self):
+        self.que = []
+        self.front = -1
+        self.length = 0
+    def qsize(self):
+        return self.length
+    def isEmpty(self):
+        return not self.length
+    def push(self,data):
+        if self.front == -1:
+            self.front = 0
+        self.que.append(data)
+        self.length += 1
+    def pop(self):
+        if self.isEmpty():
+            print("Queue empty ")
+            return
+        element = self.que[self.front]
+        self.front += 1
+        self.length -= 1
+        if self.isEmpty():
+            self.front = -1
+            self.que = []
+        return element
+
+class Solution(object):
+    def levelOrder(self, root):
+        ans = []
+        if root is None:
+            return ans
+        q = LQueue()
+        q.push(root)
+        ans.append([root.val])
+
+        while q.qsize() > 0:
+            level = []
+            l = q.qsize()
+            for i in range(l):
+                frnt = q.pop()
+                if frnt.left is not None:
+                    q.push(frnt.left)
+                    level.append(frnt.left.val)
+                if frnt.right is not None:
+                    q.push(frnt.right)
+                    level.append(frnt.right.val)
+            if len(level) > 0:
+                ans.append(level)
+        return ans
